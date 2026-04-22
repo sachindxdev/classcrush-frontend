d04e452452eef4b4f29109750edcfeb536c88d18
sachindxdev <iamsachindiwakar@gmail.com>
2026-04-22 21:25:11 +0530

    Make BASE_URL dynamic in frontend.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,2 @@
-// for Deployment
-// export const BASE_URL = "/api";
-
-// for Development
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
